16a0760fcb14a06acf50a19f3b6ad0602ca0705a
Danyllo <danyllof.gps@gmail.com>
2018-07-04 17:45:40 -0300

    Ajustes de requisicao

--- Controllers/homecontroller.php
+++ Controllers/homecontroller.php
@@ -25,29 +25,15 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $options = [
-            'headers' => [
-                'postman-token' => '5c9b3854-7ada-20a0-fcb6-3e5826bbd757',
-                'cache-control' => 'no-cache',
-                'accept-language' => 'pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7',
-                'accept-encoding' => 'gzip, deflate, br',
-                'referer' => 'https://www.psvturismo.com.br/',
-                'user-agent' => 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36',
-                'origin' => 'https://www.psvturismo.com.br',
-                'accept' => 'application/json, text/plain, */*'
-            ],
-            'form_params' => [],
-        ];
-        try {
-            $response = $this->client->request('GET', 'http://api.apigateway.test/psv/airports?limit=99999', $options);
+      try{
 
-         return $this->response_json($response->getBody());
+         return $this->response_json($this->getRequest());
 
-        }catch (\GuzzleHttp\Exception\ClientException $e){
-            echo $e->getMessage();
-        }catch (Exception $exception){
+      }catch (Exception $exception){
             echo $exception->getMessage();
         }
 
     }
+
+
 }
