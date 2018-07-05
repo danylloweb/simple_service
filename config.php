26db5762bdba38153c599b0c4d924a4823939c6d
Danyllo <danyllof.gps@gmail.com>
2018-07-05 09:32:50 -0300

    Logs

--- config.php
+++ config.php
@@ -4,9 +4,5 @@ require 'env.php';
 require 'vendor/autoload.php';
 
 return  [
-    'url'               => 'https://www.google.com/',
-    'email_send'        => 'ozymandias.mangue3@gmail.com',
-    'password'          => 'g0th@nc!ty',
-    'email_receive'     => 'iagoneresb@gmail.com'
-    //'email_receive'   => 'diretoria@psvturismo.com.br'
+    'url' => 'https://www.google.com/',
 ];
